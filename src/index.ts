f2b791f9d937fbca1fb701da2476a27109d26592
tiljrd <til.jordan@outlook.de>
2022-04-16 01:31:21 +0100

    Use commander to parse params for url and timeout

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,48 @@
 import {executeLogic, checkProvider} from "./logic";
 import * as puppeteer from 'puppeteer';
+const { program } = require('commander');
 
-(async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto('https://reuters.com');
+//Check all iframes and inject logic if it's likely to contain a pop-up
+const closeIframe = async (page) => {
 
-    //Check all iframes and inject logic if it's likely to contain a pop-up
-    const closeIframe = async () => {
-
-        //Only allow iframes with specific keywords and not the main frame
-        const isValidFrame = (frame) => {
-            let url = frame.url();
-            return frame.frameId !== 0 &&
-                (url.includes("consent") ||
+    //Only allow iframes with specific keywords and not the main frame
+    const isValidFrame = (frame) => {
+        let url = frame.url();
+        return frame.frameId !== 0 &&
+            (url.includes("consent") ||
                 url.includes("privacy") ||
                 url.includes("permission"));
-        };
-
-        //Find a valid iframe
-        let iframe = await page.frames().find(isValidFrame);
-
-        //Inject logic into a valid iframe
-        if (iframe) {
-            iframe.evaluate(executeLogic).then(iframeElement => {
-                console.log("Iframe", iframeElement);
-            });
-        }
     };
+
+    //Find a valid iframe
+    let iframe = await page.frames().find(isValidFrame);
+
+    //Inject logic into a valid iframe
+    if (iframe) {
+        let iframeElement = await iframe.evaluate(executeLogic);
+        console.log("Iframe", iframeElement);
+        return iframeElement;
+    } else {
+        return null;
+    }
+};
+
+const closePopupOnWebsite = async (url, timeout) => {
+
+    //Launch new page in Puppeteer Chromium-based browser
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+
+    //Go to the specified URL
+    await page.goto(url);
 
     //Inject logic into any valid iframes that are attached later
     page.once("frameattached", () => {
-        closeIframe();
+        closeIframe(page);
     });
 
     //Inject logic into iframe if available
-    await closeIframe();
+    await closeIframe(page);
 
     //Check if any cookie provider is active
     let providerElement = await page.evaluate(checkProvider);
@@ -46,10 +53,33 @@ import * as puppeteer from 'puppeteer';
         page.evaluate(executeLogic).then(logicElement => {
             console.log("Logic", logicElement);
         });
-
     }
 
-    await page.waitForTimeout(120000);
+    //Keep website open for a specific timeout duration
+    await page.waitForTimeout(timeout);
 
+    //Close the browser window
     await browser.close();
-})();
+};
+
+//Provide a CLI description for the core library
+program
+    .name('npm run start --')
+    .description('Stardust core library is the isolated logic for the Stardust Cookie Cutter which closes cookie' +
+        ' pop-ups and applies the correct preference. The core library uses Puppeteer so the effects of the library ' +
+        'can be seen without the full browser extension attached.')
+    .version(process.env.npm_package_version, '-v, --version', 'Output the current core version');
+
+//Specify the command line options
+program
+    .addOption(new program.Option('-u, --url <address>', 'The website URL to open and close the pop-up on')
+        .default('https://www.stardustnetwork.com/contribute', 'Stardust contribution website'))
+    .addOption(new program.Option('-t, --timeout <timeout>', 'The duration in ms to keep the browser window open')
+        .default(5000, '5 seconds'))
+    .parse();
+
+const options = program.opts();
+const url = options.url
+const timeout = options.timeout;
+
+closePopupOnWebsite(url, timeout);
